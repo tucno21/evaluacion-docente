d53a6ae9b91ec448a19816deb447a6599ac35694
tucno21 <carlitostucno@gmail.com>
2025-06-12 16:52:19 -0500

    base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App

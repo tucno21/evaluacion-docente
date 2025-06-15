77e18b38298814b684172d7e54800caa1a19ff74
tucno21 <carlitostucno@gmail.com>
2025-06-14 19:06:21 -0500

    add pwa

---
+++ public/sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'evaldocente-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/icons/icon-maskable-192x192.png',
+    '/icons/icon-maskable-512x512.png',
+    // Add other assets that should be cached, e.g., CSS, JS, images
+    // '/src/index.css',
+    // '/src/main.tsx',
+    // '/src/App.tsx',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
